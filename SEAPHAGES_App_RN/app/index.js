3cca5097232527b077b2f6089fddbb97aa5d7f6e
Melissa Wenzel <wenzelmk@dukes.jmu.edu>
2017-08-28 11:37:36 -0400

    Icons & Splash Screens
    Added Icons & Splash Screens for iOS and Android; Android splash screen
    is not currently working.

--- SEAPHAGES_App_RN/app/index.js
+++ SEAPHAGES_App_RN/app/index.js
@@ -1,8 +1,8 @@
 //Created by wenzelmk on 5/10/17.
 
-
-import React, {Component} from 'react';
-import Meteor, { createContainer } from 'react-native-meteor';
+import React, { Component } from 'react';
+import Meteor from 'react-native-meteor';
+import SplashScreen from 'react-native-splash-screen';
 
 import { Root } from './config/routes';
 import settings from './config/settings';
@@ -10,13 +10,14 @@ import settings from './config/settings';
 Meteor.connect(settings.METEOR_URL);
 
 class SEAPHAGES_App_RN extends Component {
-
+    componentDidMount() {
+        SplashScreen.hide();
+    }
 
     render() {
         return ( <Root />
         );
     }
 }
-
 
 export default SEAPHAGES_App_RN;
